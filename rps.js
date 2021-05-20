88f9c5394fd43b5a65dce2a7d113d961b4231628
Ryan Chen <ryanchen604@gmail.com>
2021-05-20 08:57:00 -0700

    add condition

---
+++ rps.js
@@ -0,0 +1,33 @@
+function computerPlay() {
+    let pOutput = ["rock", "paper", "scissors"];
+    let output = pOutput[Math.floor(Math.random() * pOutput.length)];
+    return output;
+}
+function playRound(pSelection, cSelection) {
+    if (pSelection.toLowerCase() === "rock") {
+        if (cSelection === "paper") {
+            return "c";
+        }
+        else if (cSelection === "scissors") {
+            return "p";
+        }
+    }
+}
+function game() {
+    let pScore = 0;
+    let cScore = 0;
+    for (let k = 0; k < 5; k++) {
+        let pSelection = prompt("Rock, paper, scissors!");
+        let cSelection = computerPlay();
+        let result = playRound(pSelection, cSelection);
+        if (let === "p") {
+            pScore++;
+            console.log("result: player wins!");
+        }
+        else {
+            cScore++;
+            console.log("result: computer wins!");
+        }
+        
+    }
+}
