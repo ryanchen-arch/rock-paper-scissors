47c396a0f502fcb9ccaf2501d791963dae5f58c7
ryanchen-arch <76602643+ryanchen-arch@users.noreply.github.com>
2022-03-10 13:38:59 -0800

    Update rps.js

--- rps.js
+++ rps.js
@@ -1,70 +1,84 @@
-function computerPlay() {
-    let pOutput = ["rock", "paper", "scissors"];
-    let output = pOutput[Math.floor(Math.random() * pOutput.length)];
-    return output;
-}
-function playRound(pSelection, cSelection) {
-    if (pSelection.toLowerCase() === "rock") {
-        if (cSelection === "paper") {
-            return "c";
-        }
-        else if (cSelection === "scissors") {
-            return "p";
-        }
-        else {
-            return null;
-        }
-    }
-    else if (pSelection.toLowerCase() === "paper") {
-        if (cSelection === "scissors") {
-            return "c";
-        }
-        else if (cSelection === "rock") {
-            return "p";
-        }
-        else {
-            return null;
-        }
-    }
-    else {
-        if (cSelection === "rock") {
-            return "c";
-        }
-        else if (cSelection === "paper") {
-            return "p";
-        }
-        else {
-            return null;
-        }
-    }
-}
-function game() {
+window.addEventListener('DOMContentLoaded', () => {
+    const scoreboard = document.querySelector('#scoreboard');
+    const playerS = document.querySelector('#playerS');
+    const computerS = document.querySelector('#computerS');
+    const buttons = document.querySelectorAll('button');
     let pScore = 0;
     let cScore = 0;
-    for (let k = 0; k < 5; k++) {
-        let pSelection = prompt("Rock, paper, scissors!");
-        let cSelection = computerPlay();
-        let result = playRound(pSelection, cSelection);
+    buttons.forEach((button) => {
+        button.addEventListener('click', () => {
+            
+            let cButton = computerPlay();
+            let pButton = event.target.id;
+            let result = playRound(pButton, cButton);
+            game(result);
+            
+        })
+    })
+    function computerPlay() {
+        let pOutput = ['rock', 'paper', 'scissors'];
+        let output = pOutput[Math.floor(Math.random() * pOutput.length)];
+        return output;
+    }
+    function playRound(pSelection, cSelection) {
+        if (pSelection.toString().toLowerCase() === 'rock') {
+            if (cSelection === 'paper') {
+                return 'c';
+            }
+            else if (cSelection === 'scissors') {
+                return 'p';
+            }
+            else {
+                return null;
+            }
+        }
+        else if (pSelection.toString().toLowerCase() === 'paper') {
+            if (cSelection === 'scissors') {
+                return 'c';
+            }
+            else if (cSelection === 'rock') {
+                return 'p';
+            }
+            else {
+                return null;
+            }
+        }
+        else {
+            if (cSelection === 'rock') {
+                return 'c';
+            }
+            else if (cSelection === 'paper') {
+                return 'p';
+            }
+            else {
+                return null;
+            }
+        }
+    }
+
+    function game(result) {
         if (result === "p") {
             pScore++;
-            console.log("result: player wins!");
+            playerS.textContent = 'player: ' + pScore;
+            scoreboard.textContent = 'result: player wins!';
         }
         else if (result === "c") {
             cScore++;
-            console.log("result: computer wins!");
+            computerS.textContent = 'computer: ' + cScore;
+            scoreboard.textContent = 'result: computer wins!';
         }
         else {
-            console.log("result: draw!");
+            scoreboard.textContent = 'result: draw!';
+        }
+        if (pScore === 5 || cScore === 5) {
+            if (cScore > pScore) {
+                scoreboard.textContent= "total result: computer wins!";
+            }
+            else if (pScore > cScore) {
+                scoreboard.textContent = "total result: player wins!";
+            }
         }
         
+        
     }
-    if (cScore > pScore) {
-        console.log("total result: computer wins!");
-    }
-    else if (pScore > cScore) {
-        console.log("total result: player wins!");
-    }
-    else {
-        console.log("total result: draw!");
-    }
-}
+})
